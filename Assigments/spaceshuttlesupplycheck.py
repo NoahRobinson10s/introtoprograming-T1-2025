d603b73e1fb29543c73176dba2b3cbb76e332380
Robinson Noah <827209@stma.k12.mn.us>
2025-09-25 14:03:58 -0500

    space sutlle assinment

---
+++ Assigments/spaceshuttlesupplycheck.py
@@ -0,0 +1,16 @@
+countdown = input("how many secounds there are before lanch\n>")
+countdown = input("are you sure\n>")
+oxygen_tank = input("how many oxyen tanks do you have\n>")
+oxygen_tank = input("are you sure\n>")
+food_packs = input("how many food packs do you have\n>")
+water_packs = input("how many water packs do you have\n>")
+
+#untdown = int(countdown)
+
+#xygen_tank = int(oxygen_tank)
+
+#ood_packs = int(food_packs)
+
+#ater_packs = int(water_packs)
+
+#ountdown = print("are you sure\n> + countdown") = input("how many oxyen tanks do you have\n>")
--- Practice/2.3-practice.py
+++ Practice/2.3-practice.py
@@ -9,3 +9,17 @@ uppercase = True
 camelcase = True
 SNAKE_CASE = True
 SCREAMING_SNAKE_CASE = False
+
+input("what is your name")
+print("complete")
+
+user_input = input("your name is\n")
+
+input("noah" + user_input)
+
+num = input("what number do you whant to square\n")
+
+num = int(num)
+
+print(num * num)
+
