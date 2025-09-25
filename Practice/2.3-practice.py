f2d3fba4f4416e007eea136c4adca263720fb60e
Robinson Noah <827209@stma.k12.mn.us>
2025-09-25 13:15:35 -0500

    2.3-practice

---
+++ Practice/2.3-practice.py
@@ -0,0 +1,11 @@
+# how to define a varible
+# name = value
+x=10
+y=20
+potato=6
+print(x * y * potato)
+lowercase= True
+uppercase = True
+camelcase = True
+SNAKE_CASE = True
+SCREAMING_SNAKE_CASE = False
---
+++ Practice/favmovies.py
@@ -0,0 +1,6 @@
+print("top 5 movies")
+
+print("")
+print("all quiet on the westren front")
+print("the wild robot")
+print("narvik")
