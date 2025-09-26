1e10806e3c0770c7142ccdf3669d90b6c45a0d4d
Robinson Noah <827209@stma.k12.mn.us>
2025-09-26 14:15:09 -0500

    name

--- Assigments/favroteanimal.py
+++ Assigments/favroteanimal.py
@@ -1,2 +1,17 @@
 animal = "bear"
 biome = "my grampas trofy case"
+print(animal + biome)
+
+print("thank you for takeing this ecolagy servay")
+fav_animal = input("what is your favroit animal\n>")
+animal_fact = input("fun fact about your animal\n>")
+habitat_animal = ("what is your animals habitat\n>")
+animal_eat = input("what dose your animal eat\n>")
+animmL_foodweb = input("where is your aniamal on the foodweb\n")
+animal_status = input("is your animal endangerd\n>")
+animal_pet = input("can your animal be a pet\n")
+animal_rug = input("can your animal be turned into a rug\n")
+animal_humaneat = input("can you animal be eaten safly by other humans\n")
+
+print("your favorit animal is {fav_animal} your animals fun fact {animal_fact} your animals habitat is {habitat_animal} your animals fovrit food is {animal_food} your aniamls place on the food web is {animml_foodweb} is your animal endangerd {animal_status} your aniaml is a pet or not a pet {animal_pet} your animal can be turned into a rug {animal_rug} your animal can be eaten by humans {animal_humaneat} thank you for your time for this ecolagy servay plsese donate to the park servace of your local zoo")
+
