19a92da305a8a76c91d5c286ee4c32e5abe01f4c
NoahRobinson10s <827209@mystma.org>
2025-09-29 17:08:18 -0500

    made more code

---
+++ Assigments/madlib.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+#you head to the train station to get to a train to moscow when you get to the train station you see [noun 3]
+#and you keep walking and bord the train and sit down on a [noun 4]
+#as you leave petrograd you nod off and fall into slumber
+#you awake and leave the train after it arives to the station.
+#after you get of the train you walkout of the station then you go to a horse stable a buy a horse
+#you decicde to name the hourse [noun 5]
+#soune you leave moscow to head to tsaritsyn and your first stop is ryazan.
+#it took one day 1 to arive to ryazan entering the city you buy some [noun 6] 
+#and leave the city
+#you head for the city of tsaritsyn and so you keep moveing and after near 6 days you arive to tsaritsyn.
+#you head for your house and open the door you see your family and move inside you pick up a [noun 7]
+#you soon hear about the revalution and ensure your famialy that it will be all okay.
+#sone you eat a hot bowl of [noun 8] and go to bead on [adjectve 2] and you think about your familys safty and you drift of into slumber.
+
+print("you head to the train station to get to a train to moscow when you get to the train station you see [noun 3]")
+noun 3 = input("put place for noun 3\n>")
+print("and you keep walking and borad the train and sit down on a [noun 4]")
+noun 4 = input("put place for noun 4\n>")
+print("as you leave petrograd you nod off and fall into slumber you awake and leave the train after it arives to the station. after you get of the train you walkout of the station then you go to a horse stable a buy a horse you decicde to name the hourse")
+noun 5 = input("put place for noun 5\n>")
+print("#soune you leave moscow to head to tsaritsyn and your first stop is ryazan.it took one day 1 to arive to ryazan entering the city you buy some")
+noun 6 = input("put place for noun 6\n>")
+print("and leave the city you head for the city of tsaritsyn and so you keep moveing and after near 6 days you arive to tsaritsyn.you head for your house and open the door you see your family and move inside you pick up a ")
+noun 7 = input("put place for noun 7\n>")
+print("you soon hear about the revalution and ensure your famialy that it will be all okay.sone you eat a hot bowl of")
+noun 8 = input("put place for noun 8\n>")
+print("and go to bead on ")
+adjectve 2 = input("put in place for adjective 2\n>")
+print("and you think about your familys safty and you drift of into slumber.")
