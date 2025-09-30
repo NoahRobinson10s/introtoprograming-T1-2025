995bc4415cdea8f898ca5247301f9b93703f554a
Robinson Noah <827209@stma.k12.mn.us>
2025-09-30 13:42:59 -0500

    added more story

--- Assigments/madlib.py
+++ Assigments/madlib.py
@@ -1,4 +1,10 @@
-
+#petrograd russia march 8 1917 you walk to your home and you see a noun 1
+#as you walk home you see people then more people then a crowed they where protesting the tsar
+#then you walk home and go to bed.
+#the next morning you hear from your nabor that a revolution has started worred for your family you decide to head back to tsaritsyn where you family is.
+#you decide to take the train to moscow and take a horse to tsaritsyen.
+#you walk to the train station and you see a crowd looking like adjective 1 
+#outside the train station you see noun 2 after that you head inside the train station.
 
 
 
@@ -20,18 +26,27 @@
 #you soon hear about the revalution and ensure your famialy that it will be all okay.
 #sone you eat a hot bowl of [noun 8] and go to bead on [adjectve 2] and you think about your familys safty and you drift of into slumber.
 
+print("petrograd russia march 8 1917 you walk to your home and you see a")
+noun_1 = input("put in place for noun 1\n>")
+print("as you walk home you see people then more people then a crowed they where protesting the tsar then you walk home and go to bed. the next morning you hear from your nabor that a revolution has started worred for your family you decide to head back to tsaritsyn where you family is.you decide to take the train to moscow and take a horse to tsaritsyen.you walk to the train station and you see a crowd looking like")
+adjective_1 = input("put in place for adjective 1\n>")
+print("outside the train station you see")
+noun_2 =input("put place for noun 2\n>")
+print("after that you head inside the train station.")
 print("you head to the train station to get to a train to moscow when you get to the train station you see [noun 3]")
-noun 3 = input("put place for noun 3\n>")
+noun_3 = input("put place for noun 3\n>")
 print("and you keep walking and borad the train and sit down on a [noun 4]")
-noun 4 = input("put place for noun 4\n>")
+noun_4 = input("put place for noun 4\n>")
 print("as you leave petrograd you nod off and fall into slumber you awake and leave the train after it arives to the station. after you get of the train you walkout of the station then you go to a horse stable a buy a horse you decicde to name the hourse")
-noun 5 = input("put place for noun 5\n>")
+noun_5 = input("put place for noun 5\n>")
 print("#soune you leave moscow to head to tsaritsyn and your first stop is ryazan.it took one day 1 to arive to ryazan entering the city you buy some")
-noun 6 = input("put place for noun 6\n>")
+noun_6 = input("put place for noun 6\n>")
 print("and leave the city you head for the city of tsaritsyn and so you keep moveing and after near 6 days you arive to tsaritsyn.you head for your house and open the door you see your family and move inside you pick up a ")
-noun 7 = input("put place for noun 7\n>")
+noun_7 = input("put place for noun 7\n>")
 print("you soon hear about the revalution and ensure your famialy that it will be all okay.sone you eat a hot bowl of")
-noun 8 = input("put place for noun 8\n>")
+noun_8 = input("put place for noun 8\n>")
 print("and go to bead on ")
-adjectve 2 = input("put in place for adjective 2\n>")
+adjectve_2 = input("put in place for adjective 2\n>")
 print("and you think about your familys safty and you drift of into slumber.")
+
+print(f"petrograd russia march 8 1917 you walk to your home and you see a {noun_1} as you walk home you see people then more people then a crowed they where protesting the tsar then you walk home and go to bed. the next morning you hear from your nabor that a revolution has started worred for your family you decide to head back to tsaritsyn where you family is. you decide to take the train to moscow and take a horse to tsaritsyen. you walk to the train station and you see a crowd looking like {adjective_1} outside the train station you see {noun_2} after that you head inside the train station.you head to the train station to get to a train to moscow when you get to the train station you see {noun_3} and you keep walking and bord the train and sit down on a {noun_4} as you leave petrograd you nod off and fall into slumber you awake and leave the train after it arives to the station. after you get of the train you walkout of the station then you go to a horse stable a buy a horse you decicde to name the hourse {noun_5} soune you leave moscow to head to tsaritsyn and your first stop is ryazan. it took one day 1 to arive to ryazan entering the city you buy some {noun_6} and leave the city you head for the city of tsaritsyn and so you keep moveing and after near 6 days you arive at tsaritsyn. you head for your house and open the door you see your family and move inside you pick up a {noun_7} you soon hear about the revalution and ensure your famialy that it will be all okay. sone you eat a hot bowl of {noun_8} and go to bead on {adjectve_2} and you think about your familys safty and you drift of into slumber.")
