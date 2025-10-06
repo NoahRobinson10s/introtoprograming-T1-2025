7bf40361227ef6ea411674ff516531141ffa7793
Robinson Noah <827209@stma.k12.mn.us>
2025-10-06 13:47:41 -0500

    did the unit 2 test

---
+++ Assigments/unit2testcode.py
@@ -0,0 +1,26 @@
+#question 1
+word_1 = input("what word do you what to input\n>")
+word_2 = input("what word do you what to input\n>")
+word_3 = input("what word do you what to input\n>")
+print(f"your words were {word_1},{word_2},{word_3}")
+
+#question 2
+def add_three(x,y,z):
+    print(int(x) + int(y) + int(z))
+x_1 = input("what number do you what to add\n>")
+y_1 = input("what number do you what to add\n>")
+z_1 = input("what number do you what to add\n>")
+add_three(x_1, y_1, z_1)
+
+#question 3
+def data_three():
+    print(word_qusetion_3,int(intager_question_3) + float(float_qusetion_3))
+    word_qusetion_3 = input("what word do you whant to put")
+    intager_question_3 = input("what intager do you want")
+    float_qusetion_3 = input("what float do you whant")
+
+word_qusetion_3 = input("what word do you whant to put\n>")
+intager_question_3 = input("what intager do you want\n")
+float_qusetion_3 = input("what float do you whant\n")
+
+data_three = print(word_qusetion_3,int(intager_question_3) + float(float_qusetion_3))
--- Practice/2.6functions.py
+++ Practice/2.6functions.py
@@ -27,8 +27,29 @@ def add_five_numbers():
 add_five_numbers(10,20,30,40,50)
 
 def full_name(a,b,c,d):
+    print(a + "" + b)
+a_a = input("first name\n>")
+b_d = input("input last name\n>")
 
-a = input("first name\n>")
-b = input("input last name\n>")
+full_name(a_a + b_d)
 
-full_name(a + b)
+def area_calculator(area,area_2):
+    print(area + area_2)
+
+area_calculator(10,2)
+
+def word_smash(word,smash):
+    print(str(word) + str(smash))
+word_smash("cat","dog")
+
+#print string a number of times
+#two perameters 
+def echo(word,times):
+    print(str(word)*times)
+echo("well", 1)
+
+#one paramiter
+#print happy birhtday song for there name
+def happy_birthday(name):
+    print("happy birthday to you , happy bitrthday to you,happy birthday to you" + str(name) + "happy birthday to you")
+    
